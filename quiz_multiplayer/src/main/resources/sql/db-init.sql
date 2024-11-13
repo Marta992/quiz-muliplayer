e08b3beb8aa1c58c59f9e53342f8f433f2c8e526
Marta992 <martachiofalo92@gmail.com>
2024-11-13 17:47:05 +0100

    aggiunti dati finti

--- quiz_multiplayer/src/main/resources/sql/db-init.sql
+++ quiz_multiplayer/src/main/resources/sql/db-init.sql
@@ -26,8 +26,8 @@ CREATE TABLE GAME_MATCH (
 );
 
 INSERT INTO PLAYER (NICKNAME, PASSWORD_HASH, IS_ADMIN, TOTAL_SCORE) VALUES
-("Marta", "$2a$10$cZt8jNSCTDRLStsVWX86vuuYnSQTktqWy9NmtytD3d6/hLzJUdjGW", 1, 0),
-("Paolo", "$2a$10$BwovJklOxaloflhAqPSVPuK8Qz9nPEMwmOYYyhvccp9xNvpe3TaJG", 0, 0;
+("Marta", "$2a$10$cZt8jNSCTDRLStsVWX86vuuYnSQTktqWy9NmtytD3d6/hLzJUdjGW", 1, 29),
+("Paolo", "$2a$10$BwovJklOxaloflhAqPSVPuK8Qz9nPEMwmOYYyhvccp9xNvpe3TaJG", 0, 30;
 
 INSERT INTO QUESTION (QUESTION_TEXT, CORRECT_OPTION, OTHER_OPTION_1, OTHER_OPTION_2, OTHER_OPTION_3) VALUES
 ('Chi è il fondatore della casa di Grifondoro?', 'Godric Grifondoro', 'Severus Piton', 'Salazar Serpeverde', 'Helga Tassorosso'),
@@ -40,3 +40,14 @@ INSERT INTO QUESTION (QUESTION_TEXT, CORRECT_OPTION, OTHER_OPTION_1, OTHER_OPTIO
 ('Chi ha ucciso Sirius Black?', 'Bellatrix Lestrange', 'Lucius Malfoy', 'Severus Piton', 'Nagini'),
 ('Qual è il nome dell''incantesimo che fa levitare gli oggetti?', 'Wingardium Leviosa', 'Lumos', 'Accio', 'Petrificus Totalus'),
 ('Chi è il professor di Difesa contro le Arti Oscure nel quinto anno di Harry?', 'Dolores Umbridge', 'Gilderoy Lockhart', 'Severus Piton', 'Remus Lupin');
+
+INSERT INTO GAME_MATCH (NICKNAME, SCORE, DATE_PLAYED) VALUES
+('Paolo',8,'2024-11-11 12:13:50')
+('Marta',6,'2024-11-11 13:41:15')
+('Marta',5,'2024-11-11 11:00:28')
+('Paolo',7,'2024-11-12 09:33:20')
+('Paolo',4,'2024-11-12 12:12:11')
+('Paolo',10,'2024-11-12 22:29:20')
+('Marta',9,'2024-11-13 12:22:54')
+('Marta',9,'2024-11-13 17:54:23')
+('Paolo',6,'2024-11-13 23:58:12');
